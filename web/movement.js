02794b072639e4dc541d0df2c438a7802bf486dc
akoslab <70978369+akoslab@users.noreply.github.com>
2021-12-12 19:16:08 +0100

    v1.1

--- web/movement.js
+++ web/movement.js
@@ -1,8 +1,9 @@
 let runned =0;
+let data = getElementById("input")
 
         function move(){
             console.log("clicked");
-            
+            console.log(data)
             if(runned%2==0){
                 document.getElementById("cigger").style.transform = "rotate(20deg)";
                 runned=runned+1;
